6c4fb19029e274155e1872962408f8fdd1944a0a
Srini Janarthanam <srinivasancj@gmail.com>
2017-07-10 10:31:33 +0100

    fix

--- index.js
+++ index.js
@@ -31,7 +31,28 @@ app.get('/', function (req, res) {
 
 app.post('/emc2/', function (req, res) {
     console.log(JSON.stringify(req.body));
-    console.log('Weight: ' + req.body.result.parameters.weight);
+    var weight = req.body.result.parameters.weight;
+    var m = weight.amount;
+    var weight_unit = weight.unit;
+    //convert weight into kg
+    if (weight_unit == 'g'){
+        m = m/1000.0;
+    }
+    var c2 = 9 * 10^16; //in m^2/s^2
+    var e = m * c2;
     
+    res.setHeader('Content-Type', 'application/json');
+    
+    var botSpeech = "Energy that the system can create is " + e 
+    + "Joules.";
+    
+    out = {speech: botSpeech,
+            displayText: botSpeech,
+            data: null};
+    
+    var outString = JSON.stringify(out);
+    console.log('Out:' + outString);
+    
+    res.send(outString);
 })
 
