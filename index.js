dfaddc5e41de9cac723508cce29c50dc6b6a3e79
Srini Janarthanam <srinivasancj@gmail.com>
2017-07-10 07:09:21 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+// EinsteinBot webhooks
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am EinsteinBot webhook.')
+})
+
+
+app.post('/emc2/', function (req, res) {
+    console.log(req);
+})
+
